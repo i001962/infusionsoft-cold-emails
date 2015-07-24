c15dbe2c90f64b7e30aa2066709a6db6702bef84
I001962 <i001962@gmail.com>
2015-07-24 14:09:34 -0700

    Query Infusionsoft, get tags and apply a tag.

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 // Mixing jQuery and Node.js code in the same file? Yes please!
 
-$(function(){
-	var api = require('infusionsoft-api');
+$(function() {
+  var api = require('infusionsoft-api');
 
 
-	var infusionsoft = new api.DataContext('appname', 'api-key');
-	infusionsoft.Contacts
-	    .where(Contact.FirstName, 'Kevin')
-	  //  .like(Contact.LastName, 'T%')
-	    .select(Contact.Email)
-	    .orderByDescending('LastName')
-	    .take(100)
-	    .toArray()
-	    .done(function(result) {
-	        console.log(result[0].Email);
-	    });
+  	var infusionsoft = new api.DataContext('appname', 'api-key');
+  infusionsoft.Contacts
+    .where(Contact.FirstName, 'I001962-test')
+    //  .like(Contact.LastName, 'T%')
+    .select(Contact.Email, Contact.Id)
+    .orderByDescending('LastName')
+    .take(100)
+    .toArray()
+    .done(function(result) {
+      console.log(result[0].Email + result[0].Id);
+      infusionsoft.Contacts
+        .where(Contact.Email, result[0].Email )
+        .first()
+        .then(function(contact) {
+          return infusionsoft.ContactGroupAssigns
+            .where(ContactGroupAssign.ContactId, contact.Id)
+            .toArray();
+        })
+        .then(function(cgas) {
+          cgas.forEach(function(group) {
+            console.log(group.ContactGroup, group.DateCreated);
+          });
+        });
+				    console.log(infusionsoft.ContactService
+           .addToGroup(result[0].Id, '174')
+				 );
+      // infusionsoft.ContactGroups
+      // 	.select(ContactGroup.Id)
+      // 	.take(100)
+      // 	.toArray()
+      //
+      // 	.done(function(result) {
+      // 		console.log(result[0].Id);
+      // 		});
+    });
 
+//		infusionsoft.addToGroup(ContactGroupAssign.ContactId, '174')
+		//console.log(ContactGroupAssign.ContactId + 'trying to add tag here');
 
 
 });

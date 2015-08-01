6fe86da5c4b0e451c14cc09b3a347dc21a4110a3
I001962 <i001962@gmail.com>
2015-08-01 15:37:50 -0700

    wip commit

--- script.js
+++ script.js
@@ -6,59 +6,86 @@ function displayDate() {
   console.log($('#m').val());
   console.log($('#api').val());
   document.getElementById("demo").ul = getInfsResults();
+
 }
 
 function getInfsResults() {
-//  $('#m').val('');
+  //  $('#m').val('');
   $('.demo ul').val('');
   var ul = $('.demo ul');
   var api = require('infusionsoft-api');
   var infusionsoft = new api.DataContext($('#l').val(), $('#api').val());
-  infusionsoft.Contacts
-    .where(Contact.FirstName, 'P%')
-    //  .like(Contact.LastName, 'T%')
-    .select(Contact.FirstName,Contact.LastName,Contact.Email, Contact.Id)
-    .orderByDescending('LastName')
+  var getthistag = $('#m').val() + '%';
+  //
+  //   infusionsoft.Contacts
+  // //    .where(Contact.FirstName, 'P%')
+  //     .where(Contact.Id, '4094')
+  //     //  .like(Contact.LastName, 'T%')
+  //     .select(Contact.FirstName, Contact.LastName, Contact.Email, Contact.Id)
+  //     .orderByDescending('LastName')
+  //     .take(100)
+  //     .toArray()
+  //     .done(function(result) {
+  //       for (var i = 0; i < result.length; ++i) {
+  //         console.log(result[i].Email + result[i].Id);
+  //
+  //         var li = $('<li><a target="_blank"></a></li>');
+  //
+  //         li.find('a')
+  //           .attr('href:', result[i].Email)
+  //           .text(result[i].FirstName + " " + result[i].LastName + " " + result[i].Email + " " + result[i].Id);
+  //
+  //         li.appendTo(ul);
+  //       };
+  //     });
+
+  infusionsoft.ContactGroupAssigns
+    .where(ContactGroupAssign.ContactGroup, getthistag)
+    .select(ContactGroupAssign.ContactGroup, ContactGroupAssign.ContactId)
+    .orderByDescending('ContactId')
     .take(100)
     .toArray()
     .done(function(result) {
       for (var i = 0; i < result.length; ++i) {
-        console.log(result[i].Email + result[i].Id);
-
-        var li = $('<li><a target="_blank"></a></li>');
-
-        li.find('a')
-          .attr('href:', result[i].Email)
-          .text(result[i].FirstName + " "+ result[i].LastName +" "+ result[i].Email +" "+ result[i].Id);
-
-        li.appendTo(ul);
+      //  console.log(result[i].ContactId);
+        infusionsoft.Contacts
+          .where(Contact.Id, result[i].ContactId)
+        //  .select(Contact.Id, Contact.Email)
+          .select()
+      //    .orderByDescending('Email')
+          .take(100)
+          .toArray()
+          .done(function(result2) {
+            for (var j = 0; j < result2.length; ++j) {
+              console.log(result2[i].Id + result2[i].Email);
+              var li = $('<li><a target="_blank"></a></li>');
+              li.find('a')
+                .attr('href:', result2[j].Contact.Email)
+                .text(result2[j].Id + " " + result[j].Email);
+              li.appendTo(ul);
+            }
+          });
       };
-      //
-      // infusionsoft.Contacts
-      //   .where(Contact.Email, result[0].Email)
-      //   .first()
-      //   .then(function(contact) {
-      //     return infusionsoft.ContactGroupAssigns
-      //       .where(ContactGroupAssign.ContactId, contact.Id)
-      //       .toArray();
-      //   })
-      //   .then(function(cgas) {
-      //     cgas.forEach(function(group) {
-      //       console.log(group.ContactGroup, group.DateCreated);
-      //     });
-      //   });
-      // console.log(infusionsoft.ContactService
-      //   .addToGroup(result[0].Id, '174')
-      // );
-      // infusionsoft.ContactGroups
-      // 	.select(ContactGroup.Id)
-      // 	.take(100)
-      // 	.toArray()
-      //
-      // 	.done(function(result) {
-      // 		console.log(result[0].Id);
-      // 		});
     });
-  //		infusionsoft.addToGroup(ContactGroupAssign.ContactId, '174')
-  //console.log(ContactGroupAssign.ContactId + 'trying to add tag here');
+
+//This works for first email with a tag
+    // infusionsoft.ContactGroupAssigns
+    // .where(ContactGroupAssign.ContactGroup, getthistag)
+    // .first()
+    // .then(function(contactgroupassign) {
+    //   //  console.log(infusionsoft.Contacts);
+    //     return infusionsoft.Contacts
+    //         .where(Contact.Id, contactgroupassign.ContactId)
+    //         .toArray()
+    //         .then(function(matches) {
+    //             matches.forEach(function(group) {
+    //                 console.log(group.Id, group.Email);
+    //             });
+    //         });
+    // })
+
 };
+
+
+//		infusionsoft.addToGroup(ContactGroupAssign.ContactId, '174')
+//console.log(ContactGroupAssign.ContactId + 'trying to add tag here');

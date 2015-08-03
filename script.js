b10e90b1800373ef6813be53d340a39e9d56a2e3
I001962 <i001962@gmail.com>
2015-08-03 10:58:54 -0700

    commit tags and Template selectors

--- script.js
+++ script.js
@@ -1,24 +1,95 @@
 // Using index.html for input to call Infusionsoft and find all contacts for a tag
-document.getElementById("myBtn").addEventListener("click", displayDate);
+var ul = $('.demo ul');
+var dd = $('.tagsdropdown select');
+var mySelect = $("#tagsdropdown");
+var mySelectTemplates = $("#templatesdropdown");
+var api = require('infusionsoft-api');
+var infusionsoft = new api.DataContext($('#l').val(), $('#api').val());
 
-function displayDate() {
-  console.log($('#l').val());
-  console.log($('#m').val());
-  console.log($('#api').val());
-//  document.getElementById("demo").ul = getInfsResults();
-  getInfsResults();
+$(document).ready(function() {
+  console.log("ready!");
+});
+
+document.getElementById("myBtn").addEventListener("click", displayTags);
+document.getElementById("tagsdropdown").addEventListener("change", displayContacts);
+document.getElementById("templatesdropdown").addEventListener("change", displayTemplates);
+
+function displayTags() {
+  loadtagdropdown();
+  loadtemplatedropdown()
+}
+function displayTemplates() {
+//  loadtemplatedropdown()
+}
+function displayContacts() {
+  getInfsContacts();
+}
+function loadtagdropdown() {
+  var getthistag = "%"; //select all tags
+
+  infusionsoft.ContactGroupAssigns
+    .where(ContactGroupAssign.ContactGroup, getthistag)
+    .select(ContactGroupAssign.GroupId, ContactGroupAssign.ContactGroup)
+    .orderByDescending('ContactGroup')
+    .take(1000)
+    .toArray()
+    .done(function(alltags) {
+      for (var j = 0; j < alltags.length; ++j) {
+
+        mySelect
+          .append($("<option></option>")
+            .attr("value", alltags[j].GroupId)
+            .text(alltags[j].ContactGroup));
+      }
+      var usedNames = {};
+      $("select[name='dropdowntags'] > option").each(function () {
+          if(usedNames[this.text]) {
+              $(this).remove();
+          } else {
+              usedNames[this.text] = this.value;
+          }
+      });
+});
 }
 
-function getInfsResults() {
-  //  $('#m').val('');
-  $('.demo ul').val('');
-  // var s = document.getElementById("demo").ul;
-  //           s.value = " ";
+function loadtemplatedropdown() {
+  var getthistag = "%"; //select all Templates
 
-  var ul = $('.demo ul');
-  var api = require('infusionsoft-api');
-  var infusionsoft = new api.DataContext($('#l').val(), $('#api').val());
-  var getthistag = $('#m').val() + '%';
+  infusionsoft.Templates
+    .where(Template.Id, getthistag)
+    .select(Template.Id, Template.PieceTitle, Template.Categories)
+    .orderByDescending('Categories')
+    .take(1000)
+    .toArray()
+    .done(function(alltemplates) {
+      for (var j = 0; j < alltemplates.length; ++j) {
+
+        mySelectTemplates
+          .append($("<option></option>")
+            .attr("value", alltemplates[j].Id)
+            .text(alltemplates[j].PieceTitle));
+      }
+      var usedNames = {};
+      $("select[name='dropdowntemplates'] > option").each(function () {
+          if(usedNames[this.text]) {
+              $(this).remove();
+          } else {
+              usedNames[this.text] = this.value;
+          }
+      });
+});
+}
+function getInfsContacts() {
+  var list = document.getElementById("demoul"); // Get the <ul> element with id="myList"
+  //  if (list != 'null') {
+  //  console.log(list);
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  };
+  //};
+  console.log('done clearing ULs');
+var getthistag = $( "#tagsdropdown option:selected" ).text();
+console.log(getthistag);
 
   infusionsoft.ContactGroupAssigns
     .where(ContactGroupAssign.ContactGroup, getthistag)
@@ -28,7 +99,11 @@ function getInfsResults() {
     .toArray()
     .done(function(result) {
       for (var i = 0; i < result.length; ++i) {
-    //    console.log(result[i]);
+        // console.log(result[i]);
+        // var li = $('<li><h2></h2></li>');
+        // li.find('h2')
+        //   .text(result[i].ContactGroup);
+        // li.appendTo(ul);
         infusionsoft.Contacts
           .where(Contact.Id, result[i].ContactId)
           .select(Contact.FirstName, Contact.LastName, Contact.Email, Contact.Id)
@@ -36,14 +111,14 @@ function getInfsResults() {
           .take(100)
           .toArray()
           .done(function(result2) {
-        //    console.log(result2);
+            //    console.log(result2);
             for (var j = 0; j < result2.length; ++j) {
-            var li = $('<li><a target="_blank"></a></li>');
-            li.find('a')
-              .attr('href:', result2[j].Email)
-              .text(result2[j].FirstName + " " + result2[j].LastName + " " + result2[j].Email + " " + result2[j].Id);
-            li.appendTo(ul);
-          }
+              var li = $('<li><a target="_blank"></a></li>');
+              li.find('a')
+                .attr('href:', result2[j].Email)
+                .text(result2[j].FirstName + " " + result2[j].LastName + " " + result2[j].Email + " " + result2[j].Id);
+              li.appendTo(ul);
+            }
           });
       };
     });

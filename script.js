9c7683b9fbc2ceb6b15ac98164b171b34df7a9c4
I001962 <i001962@gmail.com>
2015-08-13 23:37:01 -0700

    rough draft infs use case less actual send and tag

--- script.js
+++ script.js
@@ -6,36 +6,121 @@ var mySelectTemplates = $("#templatesdropdown");
 var api = require('infusionsoft-api');
 var infusionsoft = new api.DataContext($('#l').val(), $('#api').val());
 var mergefieldmu = [];
+var mergefieldINFSList = [];
+//mergefiledtech, mergeObject, mergeField, mergeregex
+var mergefieldvalue = [
+  [01, 02, 03, 04],
+  [10, 11, 12, 13]
+];
 var sendingfrom = "i001962@gmail.com"; // hack until config settings page
 var modesetto = "infusionsoft"; // hack until config settings page
-
+var currenttemplate = "";
+var GoogleSpreadsheet = require("google-spreadsheet");
+// spreadsheet key is the long id in the sheets URL
+var my_sheet = new GoogleSpreadsheet($('#gdoc')[0].value);
 
 $(document).ready(function() {
   console.log("ready!");
-
+  //  tinymce.init({selector:'textarea'});
 });
-document.getElementById("modedropdown").addEventListener("change", setMode);
-document.getElementById("emaildropdown").addEventListener("change", setSendFrom);
-document.getElementById("myBtn").addEventListener("click", loaddropdowns);
-document.getElementById("tagsdropdown").addEventListener("change", displayContacts);
-document.getElementById("templatesdropdown").addEventListener("change", displayTemplate);
-document.getElementById("myBtnlaunch").addEventListener("click", displayTemplate);
 
+function replaceMergefields(contactsfieldstomerge) {
+  //  console.log(contactsfieldstomerge);
+  var str = currenttemplate;
+  var newstr = "";
+  //Find each mergefiledmu in the currenttemplate and replace with contact's values
+  //str.match(/~(.*)~/g)
+  for (var k = 0; k < contactsfieldstomerge.length; ++k) {
+    if (contactsfieldstomerge[k][3] && contactsfieldstomerge[k][4]) {
+      var re = new RegExp(contactsfieldstomerge[k][3]);
+      console.log(re);
 
-function setMode() {
-  modesetto = $("#modedropdown option:selected").attr('value');
-  console.log(modesetto);
-  if (modesetto == 'standalone') {
-    console.log('mode set to standalone');
-    $("#standaloneconfig").show();
-    $("#infsconfig").hide();
-  } else {
-    console.log('mode set to infs');
-    $("#standaloneconfig").hide();
-    $("#infsconfig").show();
+      //console.log(mergefieldmu);
+
+        console.log('made it ' + contactsfieldstomerge[k][4])
+        var propertyName = contactsfieldstomerge[k][4];
+        //do something
+        str = str.replace(re, propertyName);
+      
+    };
+
+  };
+  // oranges are round, and oranges are juicy.
+  //mergefieldmu // array of mergefiles eg ~Contact.mergefields~
+  // find string in template to replace str.match(/~(.*)~/g);
+  // lookup found string from template in the mergefiledmu array
+  //if found replact the strin in the template with value form
+  // the object emailthisperson which contains the values in the same array order as
+  // must align array positions as emailthisperson will only have Contact fields not ~Link or ~User_
+  //console.log(str);
+  console.log(str);
+  currenttemplate = str;
+}
+
+function loadInfsContact(theid) {
+  var findthese = [];
+  var countfields = 0;
+  for (var k = 0; k < mergefieldvalue.length; ++k) {
+    console.log(mergefieldvalue[k][1]);
+    //Build Array of specific customer fields for INFS to return for a given contact
+    if (mergefieldvalue[k][1] == 'Contact') {
+      findthese[countfields] = mergefieldvalue[k][2];
+      countfields++;
+    };
+  };
+  var findthesefields = Object.keys(findthese).map(function(key) {
+    return findthese[key]
+  });
+  console.log(theid);
+  console.log(findthesefields);
+  infusionsoft.ContactService
+    .load(theid, findthesefields)
+    .done(function(result5) {
+      console.log(result5);
+      // add these results to the mergefieldvalue array of arrays - unbalanced
+      for (var i = 0; i < mergefieldvalue.length; ++i) {
+        if (mergefieldvalue[i][1] == 'Contact') {
+          for (var m = 0; m < findthesefields.length; ++m) {
+            if (mergefieldvalue[i][2] == findthesefields[m]) {
+              mergefieldvalue[i].push(result5[findthesefields[m]]);
+            }
+          };
+        }
+      }
+      replaceMergefields(mergefieldvalue);
+      //  sendemail(result4);
+    });
+}
+
+function getGDocContacts() {
+  removecontactlist();
+  my_sheet.getRows(1, function(err, row_data) {
+    //  console.log('pulled in ' + row_data.length + ' rows');
+    var calls = [];
+    for (var k = 0; k < row_data.length; ++k) {
+      //    console.log(row_data[k]);
+      var li = $('<li><a target="_blank"></a><input type="checkbox" id="' + row_data[k].id + '" name="sendnow" class="sendnow" value="yes"> Check to send email<br></li>');
+      li.find('a')
+        .attr('name', row_data[k].id)
+        .text(row_data[k].firstname + " " + row_data[k].lastname + " " + row_data[k].email + " " + row_data[k].id + " ");
+      li.appendTo(ul);
+      $(li[k]).bind('click', function(event) {
+        logColdEmails(event);
+      })
+    };
+  });
+}
+
+function removecontactlist() {
+  console.log('clearing email to do list');
+  var currentcontacts = document.getElementById("demoul"); // Get the <div> element with id="viewtemplate"
+  while (currentcontacts.hasChildNodes()) {
+    currentcontacts.removeChild(currentcontacts.firstChild);
+    console.log('removing child');
 
   };
 }
+
 
 
 function setSendFrom() {
@@ -46,6 +131,7 @@ function setSendFrom() {
 function loaddropdowns() {
   loadtemplatedropdown()
   loadtagdropdown();
+  //  loadmergefieldcontext(); //get 'contact' (only) mergefiled names from infs
 
 }
 
@@ -62,10 +148,12 @@ function counterZero() {
 }
 
 function sendemail(emailthisperson) {
-  console.log('you are sending meail to' + emailthisperson.target.id);
-  //  console.log(emailthisperson);
+
+  console.log('you are sending meail to' + emailthisperson + 'from email account: ' + sendingfrom);
+  console.log(emailthisperson);
   //console.log('you are sending meail to' + emailthisperson.target.fromAddress);
   var email = require("./node_modules/emailjs/email");
+  var testing = "<div>food safety scores? <br/><br/>We have 1634 of your brand's locations in our national food safety database. I'd like to hear your feedback on our scoring methodology before we launch this fall. <br/><br/>Are you available on Tuesday afternoon for a call?</div>";
   var server = email.server.connect({
     user: "i001962",
     password: "HAeGzEGtdvh34r",
@@ -75,6 +163,7 @@ function sendemail(emailthisperson) {
     }
 
   });
+  // console.log(currenttemplate);
   //		send the message and get a callback with an error or details of the message that was sent
   server.send({
     text: 'this is first test',
@@ -84,18 +173,23 @@ function sendemail(emailthisperson) {
 
     subject: "test from electron",
     attachment: [{
-      data: $("#mainContent").html(),
-      alternative: true
+      //  data: $("#mainContent").html(),
+      data: currenttemplate,
+      //  data: testing,
+      alternative: true //research sendgrid if necessary
+        //  alternative: false //research sendgrid if necessary
     }]
   }, function(err, message) {
     console.log(err || message);
   });
-  console.log($("#mainContent").html()); //see the html that was sent
+  // console.log($("#mainContent").html()); //see the html that was sent
 
 }
 
 function logColdEmails(contactidtoemail) {
-  sendemail(contactidtoemail); //  console.log(contactidtoemail.target.id);
+  //  console.log(contactidtoemail.target.id);
+  loadInfsContact(contactidtoemail.target.id);
+
   counterUp(); //increment emails sent counter for ux
   //hiding li after checkbox to send email
   $('ul li')
@@ -106,36 +200,83 @@ function logColdEmails(contactidtoemail) {
     .hide()
 }
 
-function displayTemplate() {
-
-  var currenttemplate = document.getElementById("viewtemplate"); // Get the <div> element with id="viewtemplate"
-  while (currenttemplate.hasChildNodes()) {
-    currenttemplate.removeChild(currenttemplate.firstChild);
+function cleartemplate() {
+  var cleartemplate = document.getElementById("viewtemplate"); // Get the <div> element with id="viewtemplate"
+  while (cleartemplate.hasChildNodes()) {
+    cleartemplate.removeChild(cleartemplate.firstChild);
   };
+}
 
+function findmergefields() {
+  var str = currenttemplate;
+  mergefieldmu = str.match(/~(.*)~/g);
+  mergefieldvalue = mergefieldmu;
+
+  //console.log(mergefieldvalue);
+  for (var j = 0; j < mergefieldmu.length; ++j) {
+    // split to find INFS object eg Customer and field names eg email
+    var splitmergefields = mergefieldmu[j].match(/[^~.]+/g);
+    //    mergefieldvalue[j] = [mergefieldmu[j], splitmergefields[0], splitmergefields[1], " "];
+    //build the regex needed to replace token in template for each mergefieldvalue
+    var re = new RegExp(mergefieldmu[j], "g");
+    //  console.log(re);
+
+    //  Object.keys(myHash).map(function(key) { return [key, myHash[key]]; })
+    mergefieldvalue[j] = [mergefieldmu[j], splitmergefields[0], splitmergefields[1], re];
+
+    // if (splitmergefields[0] == 'Contact') {
+    //   mergefieldINFSList[j] = splitmergefields[1];
+    // }
+  };
+  console.log(mergefieldvalue);
+  //flatten object to array
+  // var arr = Object.keys(mergefieldINFSList).map(function(key) {
+  //   return mergefieldINFSList[key]
+  // });
+  // mergefieldINFSList = arr;
+  // console.log(mergefieldINFSList);
+}
+
+
+function displayTemplate() {
+  cleartemplate();
   var getthistemplate = $("#templatesdropdown option:selected").attr("value");
-
   infusionsoft.APIEmailService
     .getEmailTemplate(getthistemplate)
     .done(function(result3) {
-      console.log(result3);
+      //    console.log(result3);
       $(".viewtemplate").append(result3.htmlBody);
       //    $( ".viewtemplate" ).append( result3.htmlBody );
-      //    $( "#viewtemplate" ).val( result3.htmlBody );
+      // strip can spam from display area as well as in global variable
       $(".viewtemplate").find('tr').last().remove();
+      currenttemplate = $(".viewtemplate").html();
+      findmergefields();
+
     });
 }
 
-function loadmergefieldcontext(mergefieldmu) {
-  infusionsoft.APIEmailService
-    .getAvailableMergeFields("Contact")
-    .done(function(allmfs) {
-      //      console.log(allmfs);
-    });
-}
+
+// function loadmergefieldcontext() {
+//   infusionsoft.APIEmailService
+//     .getAvailableMergeFields("Contact")
+//     .done(function(allmfs) {
+//       //    console.log(allmfs);
+//       for (var j = 0; j < allmfs.length; ++j) {
+//         //  console.log(allmfs[j])
+//         mergefieldmu[allmfs[j]] = 'not-set';
+//       }
+//       //   console.log(mergefieldmu);
+//     });
+//
+// }
 
 function displayContacts() {
   getInfsContacts();
+  counterZero();
+}
+
+function displayContactsGDoc() {
+  getGDocContacts();
   counterZero();
 }
 
@@ -146,7 +287,7 @@ function loadtagdropdown() {
     .where(ContactGroupAssign.ContactGroup, getthistag)
     .select(ContactGroupAssign.GroupId, ContactGroupAssign.ContactGroup)
     .orderByDescending('ContactGroup')
-    .take(1000)
+    .take(10000)
     .toArray()
     .done(function(alltags) {
       for (var j = 0; j < alltags.length; ++j) {
@@ -168,7 +309,7 @@ function loadtagdropdown() {
 }
 
 function loadtemplatedropdown() {
-  loadmergefieldcontext(mergefieldmu);
+
   //  var getthistag = "%"; //select all Templates
   infusionsoft.Templates
     //  .where(Template.Id, getthistag)
@@ -198,14 +339,8 @@ function loadtemplatedropdown() {
 }
 
 function getInfsContacts() {
-  var list = document.getElementById("demoul"); // Get the <ul> element with id="myList"
-  //  if (list != 'null') {
-  //  console.log(list);
-  while (list.hasChildNodes()) {
-    list.removeChild(list.firstChild);
-  };
-  //};
-  //  console.log('done clearing ULs');
+  removecontactlist();
+
   var getthistag = $("#tagsdropdown option:selected").text();
   //  console.log(getthistag);
 
@@ -217,11 +352,6 @@ function getInfsContacts() {
     .toArray()
     .done(function(result) {
       for (var i = 0; i < result.length; ++i) {
-        // console.log(result[i]);
-        // var li = $('<li><h2></h2></li>');
-        // li.find('h2')
-        //   .text(result[i].ContactGroup);
-        // li.appendTo(ul);
         infusionsoft.Contacts
           .where(Contact.Id, result[i].ContactId)
           .select(Contact.FirstName, Contact.LastName, Contact.Email, Contact.Id)
@@ -231,17 +361,14 @@ function getInfsContacts() {
           .done(function(result2) {
             var calls = [];
             for (var j = 0; j < result2.length; ++j) {
-              var li = $('<li><a target="_blank"></a><input type="checkbox" id="' + result2[j].Email + '" name="sendnow" class="sendnow" value="yes"> Check to send email<br></li>');
+              var li = $('<li><a target="_blank"></a><input type="checkbox" id="' + result2[j].Id + '" name="sendnow" class="sendnow" value="yes"> Check to send email<br></li>');
               li.find('a')
-                .attr('name', result2[j].Email)
+                .attr('name', result2[j].Id)
                 .text(result2[j].FirstName + " " + result2[j].LastName + " " + result2[j].Email + " " + result2[j].Id + " ");
+              li.find('input')
+                .attr('name', result2[j].Id);
+
               li.appendTo(ul);
-              // calls[j] = function() {
-              //   console.log('Sent for you');
-              //   logColdEmails(result2[j].Id);
-              //   //  cleanupLi();
-              // };
-              //    console.log('before event handler');
               $(li[j]).bind('click', function(event) {
                 logColdEmails(event);
               });
@@ -250,6 +377,48 @@ function getInfsContacts() {
       };
     });
 };
+
+function showProps(obj, objName) {
+  var result = "";
+  //  console.log(obj);
+  for (var i in obj) {
+    if (obj.hasOwnProperty(i)) {
+      //result += obj[i];
+      console.log(i);
+      result += i + ',';
+    }
+  }
+  return result;
+}
+
+function setMode() {
+  removecontactlist();
+  modesetto = $("#modedropdown option:selected").attr('value');
+  //  console.log(modesetto);
+
+  if (modesetto == 'standalone') {
+    //    console.log('mode set to standalone');
+    $("#standaloneconfig").show();
+    $("#infsconfig").hide();
+    $("#viewtemplate").hide();
+    currenttemplate = "";
+  } else {
+    //    console.log('mode set to infs');
+    $("#standaloneconfig").hide();
+    $("#infsconfig").show();
+    $("#viewtemplate").show();
+    currenttemplate = "";
+    //  loadmergefieldcontext();
+
+  };
+}
+
+document.getElementById("modedropdown").addEventListener("change", setMode);
+document.getElementById("emaildropdown").addEventListener("change", setSendFrom);
+document.getElementById("myBtn").addEventListener("click", loaddropdowns);
+document.getElementById("tagsdropdown").addEventListener("change", displayContacts);
+document.getElementById("templatesdropdown").addEventListener("change", displayTemplate);
+document.getElementById("myBtnlaunch").addEventListener("click", displayContactsGDoc);
 
 //		infusionsoft.addToGroup(ContactGroupAssign.ContactId, '174')
 //console.log(ContactGroupAssign.ContactId + 'trying to add tag here');
